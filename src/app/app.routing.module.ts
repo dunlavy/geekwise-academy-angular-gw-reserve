7a53eea7f7308997428f52b75e7fa329ce440388
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-07 22:30:45 -0700

    feature: introduce navigation component to application

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import ComponentOne from "./component-one/component-one";
-import ComponentTwo from "./component-two/component-two";
 import FourOhFourComponent from "./404-component/404-component";
 
 const routes:Routes = [
 	{
-		path: "c1",
-		component: ComponentOne
-	},
-	{
-		path: "c2",
-		component: ComponentTwo
-	},
-	{
 		path: "",
-		component: ComponentOne
+		component: FourOhFourComponent
+	},
+	{
+		path: "welcome",
+		component: FourOhFourComponent
+	},
+	{
+		path: "rooms/:id",
+		component: FourOhFourComponent
 	},
 	{
 		path: "**",
@@ -35,7 +33,5 @@ const routes:Routes = [
 export class AppRoutingModule { }
 
 export const routedComponents = [
-	ComponentOne,
-	ComponentTwo,
 	FourOhFourComponent
 ];
