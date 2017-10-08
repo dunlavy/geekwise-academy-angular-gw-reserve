beb65cfe45147aff1d7e8240c360867b139f9b71
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-08 00:27:37 -0700

    feature: add welcome component, configure routing

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import WelcomeComponent from "./components/welcome/welcome.component";
 import FourOhFourComponent from "./components/404/404.component";
 import RoomComponent from "./components/room/room.component";
 
 const routes:Routes = [
 	{
 		path: "",
-		component: FourOhFourComponent
+		component: WelcomeComponent
 	},
 	{
 		path: "welcome",
-		component: FourOhFourComponent
+		component: WelcomeComponent
 	},
 	{
 		path: "rooms/:id",
@@ -34,5 +35,5 @@ const routes:Routes = [
 export class AppRoutingModule { }
 
 export const routedComponents = [
-	FourOhFourComponent, RoomComponent
+	WelcomeComponent, FourOhFourComponent, RoomComponent
 ];
