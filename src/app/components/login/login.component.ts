bd2d3a854eb2c5bf8bd265e9e820cd80b66f7065
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-08 18:33:35 -0700

    feature: update login component to use login service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,22 @@
 import { Component } from "@angular/core";
+import { LoginService } from "./../../services/login.service";
 
 @Component({
 	selector: "gw-login",
 	templateUrl: "./login.component.html"
 })
-export default class LoginComponent { }
+export default class LoginComponent {
+	constructor(private _loginService: LoginService) { }
+
+	public login() {
+		this._loginService.login();
+	}
+
+	public logout() {
+		this._loginService.logout();
+	}
+
+	public get loggedInUser() {
+		return this._loginService.getLoggedInUser();
+	}
+}
