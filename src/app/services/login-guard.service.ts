7938a19ab02489aa64a3d12ad0542209cb02ac35
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-12 13:34:41 -0700

    feature: update login-service to send to welcome route with fragment

--- src/app/services/login-guard.service.ts
+++ src/app/services/login-guard.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from "@angular/core";
-import { CanActivate } from "@angular/router";
+import { CanActivate, Router } from "@angular/router";
 import { LoginService } from "./login.service";
 
 @Injectable()
 export class LoginGuardService implements CanActivate {
-	constructor(private _loginService: LoginService) { }
+	constructor(
+		private _loginService: LoginService,
+		private _router: Router
+	) { }
 
 	canActivate() {
-		return Boolean(this._loginService.getLoggedInUser());
+		const isLoggedIn = Boolean(this._loginService.getLoggedInUser());
+
+		if (!isLoggedIn) this._router.navigate(["welcome"], { fragment: "show-warning" });
+
+		return isLoggedIn;
 	}
 }
