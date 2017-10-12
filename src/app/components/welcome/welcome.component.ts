986703656c6079f431b0fb578afc629a3278ff99
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-12 13:15:17 -0700

    chore: remove unused import... doh!

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 
-import { INavigationItem } from "./../../interfaces/INavigationItem";
-
 @Component({
 	selector: "gw-welcome",
 	templateUrl: "./welcome.component.html",
-	styleUrls: [
-		"./welcome.component.css"
-	]
+	styleUrls: [ "./welcome.component.css" ]
 })
 export default class WelcomeComponent implements OnInit {
 	ngOnInit() { }
