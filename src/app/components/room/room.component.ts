63119f23a159ced1c38e511188cb3bffa56e493a
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-24 13:56:52 -0700

    refactor: room component now uses observable from room service and switches room thus

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
+import { RoomService } from "./../../services/room.service";
+
 @Component({
 	selector: "gw-room",
 	templateUrl: "./room.component.html",
 	styleUrls: ["./room.component.css"]
 })
 export default class RoomComponent implements OnInit {
-	public roomId:string;
+	public room;
 
-	constructor(private _activatedRoute:ActivatedRoute) {	}
+	constructor(
+		private _activatedRoute:ActivatedRoute,
+		private _roomService:RoomService
+	) { }
 
 	public ngOnInit() {
-		this._activatedRoute.paramMap.subscribe(route => {
-			this.roomId = route.get("id");
-		});
+		this._activatedRoute.paramMap.subscribe(route => this._switchRoom(route.get("id")));
+	}
+
+	private _switchRoom(id:string) {
+		this._roomService.getRoomById(id).subscribe(room => { this.room = room });
 	}
 }
