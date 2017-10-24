6531ecf7b85ad8daa981ba38c7b9745531e2c4ea
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-24 01:30:29 -0700

    refactor: import and use room service

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,34 +1,44 @@
 import { Component, OnInit } from "@angular/core";
 
+import { Observable } from "rxjs/Observable";
+
 import { INavigationItem } from "./../../interfaces/INavigationItem";
+import { IRoom } from "./../../interfaces/IRoom";
+import { RoomService } from "./../../services/room.service";
 
 @Component({
 	selector: "gw-navigation",
 	templateUrl: "./navigation.component.html",
-	styleUrls: [
-		"./navigation.component.css"
-	]
+	styleUrls: ["./navigation.component.css"]
 })
 export default class NavigationComponent implements OnInit {
 	public navigationItems:INavigationItem[];
 
-	ngOnInit() {
-		// before we consume a remote data source, we'll mock up data for our
-		// navigation menu to bind to
+	constructor(private _roomService:RoomService) { }
 
-		this.navigationItems = [
-			{
-				title: "Welcome",
-				url: "/welcome"
-			},
-			{
-				title: "Room 1",
-				url: "/rooms/1"
-			},
-			{
-				title: "Room 2",
-				url: "/rooms/2"
-			}
-		];
+	ngOnInit() {
+		this.navigationItems = [];
+
+		this.navigationItems.push({
+			color: "blue",
+			title: "Welcome",
+			url: "/welcome"
+		});
+
+		this._roomService.roomsObservable
+			.map(rooms => {
+				return rooms.map(room => {
+					const navigationItem:INavigationItem = {
+						color: "green",
+						title: room.name,
+						url: "/rooms/" + room.id
+					};
+
+					return navigationItem;
+				});
+			})
+			.subscribe(rooms => {
+				this.navigationItems.concat(rooms);
+			});
 	}
 }
