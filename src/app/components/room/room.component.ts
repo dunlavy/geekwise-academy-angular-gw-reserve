2a9522309cb0a177e5854cebf368c9f2b397c4e2
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-24 01:50:30 -0700

    chore: remove old comment

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -12,10 +12,6 @@ export default class RoomComponent implements OnInit {
 	constructor(private _activatedRoute:ActivatedRoute) {	}
 
 	public ngOnInit() {
-		// my parent "room.component" is going to do all teh work getting room, so let's ensure
-		// this hard work doesn't go to waste in our child components by fetching the same
-		// information in them...
-
 		this._activatedRoute.paramMap.subscribe(route => {
 			this.roomId = route.get("id");
 		});
