46f8dcf41229a0898cb5957fcd953c2ddf1df888
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-25 14:57:45 -0700

    feature: change interfaces related to reservations

--- src/app/components/room/room-reactive-form/IReservationForm.ts
+++ src/app/components/room/room-reactive-form/IReservationForm.ts
@@ -1,10 +1,8 @@
-import { IReservation } from "./../../../interfaces/IReservation";
-
 export interface IReservationForm {
 	email: string | any[],
 	reason: string | any[],
-	startDateTime: string | any[],
-	endDateTime: string | any[],
+	startTime: string | any[],
+	endTime: string | any[],
 	emailConfirmation: string | any[],
 	isAgreed: string | any[]
 }
