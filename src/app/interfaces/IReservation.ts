f1b8c60098d6ace5e89dbdc94b3ddc91eaa2973d
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-29 21:15:16 -0700

    chore: let any property exist on IReservation, fix dumb syntax mistake

--- src/app/interfaces/IReservation.ts
+++ src/app/interfaces/IReservation.ts
@@ -1,9 +1,10 @@
 export interface IReservation {
-	id?: string,
-	email: string,
-	reason: string,
-	startTime: string,
-	endTime: string,
-	emailConfirmation: string,
-	isAgreed: string
+	id?: string;
+	email: string;
+	reason: string;
+	startTime: string;
+	endTime: string;
+	emailConfirmation: string;
+	isAgreed: string;
+	[propName: string]: any;
 }
