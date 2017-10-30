cd621946d4f7f5b1261edf77c123107759218e30
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-10-29 21:17:08 -0700

    feature: animate room list items

--- src/app/components/room/room-list/room-list.component.ts
+++ src/app/components/room/room-list/room-list.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { trigger, state, style, animate, transition } from "@angular/animations";
 
 import { IRoom } from "./../../../interfaces/IRoom";
 
 import { RoomService } from "./../../../services/room.service";
 
+const animations = [
+	trigger("onLoad", [
+		state("init", style({
+			transform: "translateX(-10%)",
+			opacity: 0
+		})),
+		state("complete", style({
+			transform: "translateX(0)",
+			opacity: 1
+		})),
+		transition("init => complete", animate("150ms ease-out"))
+	])
+];
+
 @Component({
 	selector: "gw-room-list",
 	templateUrl: "./room-list.component.html",
-	styleUrls: ["./room-list.component.css"]
+	styleUrls: ["./room-list.component.css"],
+	animations
 })
 export default class RoomListComponent implements OnInit {
 	public room:IRoom;
@@ -27,6 +43,16 @@ export default class RoomListComponent implements OnInit {
 	}
 
 	private _switchRoom(id: string) {
-		this._roomService.getRoomById(id).subscribe(room => { this.room = room; });
+		this._roomService.getRoomById(id)
+			.map(room => {
+				room.reservations.forEach((reservation, index) => {
+					setTimeout(() => {
+						reservation.isTadaTime = true;
+					}, 100 * index);
+				});
+
+				return room;
+			})
+			.subscribe(room => { this.room = room });
 	}
 }
