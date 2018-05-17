ab48233ee79d3b90607e3314c4e4aabc875a7e16
Josh R. Dunlavy <jdunlavy@editllc.com>
2018-05-17 11:12:51 -0700

    chore: install new API keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,12 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
 	production: false,
 
 	firebaseConfiguration: {
-		apiKey: "AIzaSyDhJPjpVUCmCMvw6DTaTlorDLnE1Enh4Wg",
-		authDomain: "confdeconflictor.firebaseapp.com",
-		databaseURL: "https://confdeconflictor.firebaseio.com",
-		projectId: "confdeconflictor",
-		storageBucket: "confdeconflictor.appspot.com",
-		messagingSenderId: "519140165558"
+		apiKey: "AIzaSyByBwBX4_u44Y7KZSkuqOZGn6M0hoCS-PQ",
+		authDomain: "geekwise-angular.firebaseapp.com",
+		databaseURL: "https://geekwise-angular.firebaseio.com",
+		projectId: "geekwise-angular",
+		storageBucket: "geekwise-angular.appspot.com",
+		messagingSenderId: "623327886979"
 	}
 };
